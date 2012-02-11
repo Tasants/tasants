5a46cab5d5266557e6b4b6884aa00f3ac2ad78ba
Tasants <contact.tasants@gmail.com>
2012-02-11 12:13:42 +0200

    Better test for Mnet parsin + new data.

--- tests/unit/lib/parsers/EventImportMnetTest.php
+++ tests/unit/lib/parsers/EventImportMnetTest.php
@@ -4,6 +4,28 @@ class EventImportMnetTest extends TasantsTestCase {
     public function testParsing() {
         $service = new EventImportService();
         $events = $service->Parse(new EventImportMnet());
-        $this->assertEquals(564, sizeof($events));
+        $this->assertEquals(568, sizeof($events));
+
+        $event = $events[0];
+        /* @var $event EventData */
+        $this->assertEquals("Afrocola", $event->Name());
+        $this->assertEquals("ravintola juttutupa", $event->Place());
+        $this->assertEquals("Helsinki", $event->City());
+        $this->assertEquals("11.02.2012", $event->Date());
+
+        $event = $events[1];
+        /* @var $event EventData */
+        $this->assertEquals("Antero Lindgren", $event->Name());
+        $this->assertEquals("Bar Loose", $event->Place());
+        $this->assertEquals("Helsinki", $event->City());
+        $this->assertEquals("11.02.2012", $event->Date());
+
+        $event = $events[567];
+        /* @var $event EventData */
+        $this->assertEquals("Grazy Mama & The Sidekicks", $event->Name());
+        $this->assertEquals("Ravintola Rubiini", $event->Place());
+        $this->assertEquals("Helsinki", $event->City());
+        $this->assertEquals("25.05.2012", $event->Date());
+
     }
 }
